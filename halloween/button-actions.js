5e692d141c4bdfe9530f46a7250128788a6d93ba
watson-nm <57500106+watson-nm@users.noreply.github.com>
2020-10-31 14:44:48 -0400

    buttons

--- halloween/button-actions.js
+++ halloween/button-actions.js
@@ -1,8 +1,8 @@
 
 function run_away() {
-  return "you run further into the woods trying to escape the terror"
+  alert("you run further into the woods trying to escape the terror");
 }
 
 function hide() {
-  return "you quickly hide yourself, closing your eyes against the horrors that lurk beyond"
+  alert("you quickly hide yourself, closing your eyes against the horrors that lurk beyond");
 }
