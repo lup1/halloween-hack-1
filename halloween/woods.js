11e22126d17c563026857545983071c8d1dc0d77
watson-nm <57500106+watson-nm@users.noreply.github.com>
2020-10-31 13:02:43 -0400

    Create woods.js

---
+++ halloween/woods.js
@@ -0,0 +1,23 @@
+class light {
+  constructor(big){
+    this.big = big;
+  }
+};
+
+class entities {
+  constructor(name, fear){
+    this.name = name;
+    this.fear = fear;
+  }
+};
+
+class bright_shadow extends entities {
+  constructor(name, fear, speed) {
+    super(name, fear);
+    this.speed = speed;
+  }
+};
+
+var SpookyWoods = {
+  whisp = light(100)
+};
