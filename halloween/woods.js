2ee918bfd84746afae3ed399f81e3a8c67ec41d8
watson-nm <57500106+watson-nm@users.noreply.github.com>
2020-10-31 13:39:02 -0400

    the woods have things in them

--- halloween/woods.js
+++ halloween/woods.js
@@ -5,20 +5,30 @@ class light {
 };
 
 class entities {
-  constructor(name, fear){
+  constructor(name, description, fear) {
     this.name = name;
     this.fear = fear;
+    this.description = description;
   }
 };
 
 class bright_shadow extends entities {
-  constructor(name, fear, speed) {
-    super(name, fear);
+  constructor(name, description, fear, speed) {
+    super(name, description, fear);
     this.speed = speed;
   }
 };
 
+class safety {
+  constructor(name, description, safe) {
+    this.name = name;
+    this.safe = safe;
+    this.description = description;
+  }
+}
+
 var SpookyWoods = [
   whisp = new light(100),
-  floater = bright_shadow("glowing shadow", 2, 10)
+  floater = bright_shadow("glowing shadow", "A fleeting shadow that somehow glows with unearthly darkness", 2, 10),
+  tree = new safety("ancient oak", "its roots create an alcove for you to hide in", 2)
 ];
