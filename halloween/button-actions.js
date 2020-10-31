0d6a7931f1f2db21232f7e570820aa308d7014ea
watson-nm <57500106+watson-nm@users.noreply.github.com>
2020-10-31 13:56:50 -0400

    Update button-actions.js

---
+++ halloween/button-actions.js
@@ -0,0 +1,8 @@
+
+function run_away() {
+  return "you run further into the woods trying to escape the terror"
+}
+
+function hide() {
+  return "you quickly hide yourself, closing your eyes against the horrors that lurk beyond"
+}
