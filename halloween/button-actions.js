9db9e45f39a604b272901ad8ce4eb1ea4d8a7158
watson-nm <57500106+watson-nm@users.noreply.github.com>
2020-10-31 15:52:21 -0400

    more button

--- halloween/button-actions.js
+++ halloween/button-actions.js
@@ -1,8 +1,10 @@
+document.getElementById("run").onclick = run_away();
+document.getElementById("hide").onclick = hide();
 
 function run_away() {
-  alert("you run further into the woods trying to escape the terror");
+  document.getElementById("the-words").innerHTML = "you run further into the woods";
 }
 
 function hide() {
-  alert("you quickly hide yourself, closing your eyes against the horrors that lurk beyond");
+  document.getElementById("the-words").innerHTML = "you quickly hide yourself, closing your eyes against the horrors that lurk beyond";
 }
