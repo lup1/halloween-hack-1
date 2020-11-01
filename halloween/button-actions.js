7dce75b2f78db9ba4d95345c6888a4c3a7b95354
watson-nm <57500106+watson-nm@users.noreply.github.com>
2020-10-31 20:30:07 -0400

    spooky stuff

--- halloween/button-actions.js
+++ halloween/button-actions.js
@@ -1,10 +1,54 @@
 document.getElementById("run").onclick = run_away();
 document.getElementById("hide").onclick = hide();
+document.getElementById("area").onclick = area();
 
 function run_away() {
-  document.getElementById("the-words").innerHTML = "you run further into the woods";
+  document.getElementById("the-words1").innerHTML = "you run further into the woods";
+  clear();
 }
 
 function hide() {
-  document.getElementById("the-words").innerHTML = "you quickly hide yourself, closing your eyes against the horrors that lurk beyond";
+  document.getElementById("the-words1").innerHTML = "you quickly hide yourself, closing your eyes against the horrors that lurk beyond";
+  clear();
+}
+
+function area() {
+  choice = Math.floor(Math.random() * 2);
+  danger = Math.floor(Math.random() * 2);
+
+  if (choice == 0) {
+    desc = "you are in a glade. the grass beneath your feet is frozen, and every step seems to crack as loud as a gunshot. the trees around you sway in a breez you cannot feel";
+    document.getElementById("the-words1").innerHTML = desc;
+    if (danger == 1) {
+      document.getElementById("the-words2").innerHTML = "in the middle of the clearing you see a crouched figure. they slowly stand, and your breath catches as they";
+      document.getElementById("the-words3").innerHTML = "just";
+      document.getElementById("the-words4").innerHTML = "keep";
+      document.getElementById("the-words5").innerHTML = "getting";
+      document.getElementById("the-words6").innerHTML = "taller";
+    } else {
+      clear();
+    }
+  }
+
+  if (choice == 1) {
+    desc = "before you stands a cabin. or is it a castle? before your blurry eyes it seems to warp. one moment delapidated, the next resplendent. the smell of blood stays";
+    document.getElementById("the-words1").innerHTML = desc;
+    if (danger == 1) {
+      document.getElementById("the-words2").innerHTML = "the door is open and your hear music";
+      document.getElementById("the-words3").innerHTML = "a hag-";
+      document.getElementById("the-words4").innerHTML = "a queen-";
+      document.getElementById("the-words5").innerHTML = "a thing with many eyes and an impossible smile calls your name";
+      document.getElementById("the-words6").innerHTML = "";
+    } else {
+      clear();
+    }
+  }
+}
+
+function clear() {
+  document.getElementById("the-words2").innerHTML = "";
+  document.getElementById("the-words3").innerHTML = "";
+  document.getElementById("the-words4").innerHTML = "";
+  document.getElementById("the-words5").innerHTML = "";
+  document.getElementById("the-words6").innerHTML = "";
 }
