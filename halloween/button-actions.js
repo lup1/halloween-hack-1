9fd07bc88060e6f159741332e9f8b20eab4f7f7b
watson-nm <57500106+watson-nm@users.noreply.github.com>
2020-10-31 22:31:45 -0400

    Update button-actions.js

--- halloween/button-actions.js
+++ halloween/button-actions.js
@@ -12,7 +12,7 @@ function hide() {
 }
 
 function area() {
-  choice = Math.floor(Math.random() * 2);
+  choice = Math.floor(Math.random() * 3);
   danger = Math.floor(Math.random() * 2);
 
   if (choice == 0) {
@@ -44,6 +44,21 @@ function area() {
       clear();
     }
   }
+
+  if (choice == 2) {
+    desc = "there is a river in your path. stones jut up out of the water. it is risky but you must continue on.";
+    document.getElementById("the-words1").innerHTML = desc;
+    if (danger == 1) {
+      document.getElementById("the-words2").innerHTML = "the water is dark and fast";
+      document.getElementById("the-words3").innerHTML = "";
+      document.getElementById("the-words4").innerHTML = "the river is not wide, but it is so, so deep";
+      document.getElementById("the-words5").innerHTML = "";
+      document.getElementById("the-words6").innerHTML = "something is splashing to your left";
+      //options, ignore it or look. otherworldly singing starts when you click?
+    } else {
+      clear();
+    }
+  }
 }
 
 function clear() {
@@ -62,6 +77,7 @@ function clearingDeath() {
   document.getElementById("the-words4").innerHTML = "infront";
   document.getElementById("the-words5").innerHTML = "";
   document.getElementById("the-words6").innerHTML = "of you";
+  //delayed gif or picture, plays after 10s
 }
 
 function clearingLive() {
@@ -74,5 +90,37 @@ function clearingLive() {
 }
 
 function cabinDeath() {
+  desc = "you are drawn forward towards the beutiful terrible thing";
+  document.getElementById("the-words2").innerHTML = "the castle is grand";
+  document.getElementById("the-words3").innerHTML = "the queen has a smile like stained glass";
+  document.getElementById("the-words4").innerHTML = "it is so warm";
+  document.getElementById("the-words5").innerHTML = "and you are so tired";
+  document.getElementById("the-words6").innerHTML = "what can it hurt?";
+}
 
+function cabinLife() {
+  desc = "how does it know your name? you have taken care not to speak it in these woods.";
+  document.getElementById("the-words2").innerHTML = "you call out and ask how it knows you";
+  document.getElementById("the-words3").innerHTML = "";
+  document.getElementById("the-words4").innerHTML = "the smile drops";
+  document.getElementById("the-words5").innerHTML = "";
+  document.getElementById("the-words6").innerHTML = "the door slams closed and the little wooden shack no longer wavers as if seen through the haze of heated air";
+}
+
+function riverDeath() {
+  desc = "you glance over and are entranced";
+  document.getElementById("the-words2").innerHTML = "they are singing to you in a thousand voices, moonlight is reflected their wide pale eyes";
+  document.getElementById("the-words3").innerHTML = "your vison is blurring and your ears being to ring";
+  document.getElementById("the-words4").innerHTML = "you must keep listing, you cant let the music fade!";
+  document.getElementById("the-words5").innerHTML = "";
+  document.getElementById("the-words6").innerHTML = "your foot sinks into the inky darkness";
+}
+
+function riverLife() {
+  desc = "you step to the next rock. as you hear the first low croon you clap your hands over your ears.";
+  document.getElementById("the-words2").innerHTML = "they are loud but not louder the sound of your blood";
+  document.getElementById("the-words3").innerHTML = "step by step you make your way across the river";
+  document.getElementById("the-words4").innerHTML = "you hum an old lullaby to yourself and it keeps you grounded";
+  document.getElementById("the-words5").innerHTML = "";
+  document.getElementById("the-words6").innerHTML = "they cannot follow you as you walk from the shore to the trees";
 }
